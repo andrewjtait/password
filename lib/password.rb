87cb545b807da42e85a830ab86473248799e87cb
Andrew Tait <ajtait.uk@gmail.com>
2020-09-06 19:47:18 +0100

    Improve remaining character calc and selection
    Refactor the code so that remaining characters is more robust, taking
    into account if length argument is set or if the sum of the other
    arguments are more than the default length.
    
    This change also updates the character selection to extract a single
    character at a time using sample, rather than selecting all characters
    at once. This was problematic as letters could only be used once, now we
    can have the same letter multiple times.

--- lib/password.rb
+++ lib/password.rb
@@ -1,48 +1,68 @@
 module Password
+  class InvalidOptions < StandardError
+    def message
+      "Options are not compatible with given value of length."
+    end
+  end
+
   DEFAULT_LENGTH = 15
-  UPPERCASE = [*'A'..'Z'].freeze
+  UPPERCASE = [*"A".."Z"].freeze
   LOWERCASE = [*"a".."z"].freeze
   NUMBERS = [*0..9].freeze
   SPECIAL = %w(! @ £ $ % ^ & * #).freeze
 
-  def self.generate(length: DEFAULT_LENGTH, lowercase: nil, uppercase: nil, numbers: nil, special: nil)
-    options = build_options(length, lowercase, uppercase, numbers, special)
-
-    characters = LOWERCASE.sample(options[:lowercase]) +
-                 UPPERCASE.sample(options[:uppercase]) +
-                 NUMBERS.sample(options[:numbers]) +
-                 SPECIAL.sample(options[:special])
-
-    characters.shuffle.join
-  end
-
-  def self.build_options(length, lowercase, uppercase, numbers, special)
-    remaining_characters = length - lowercase.to_i - uppercase.to_i - numbers.to_i - special.to_i
-
-    raise StandardError if remaining_characters < 0
-
-    user_defined_options = {
+  def self.generate(length: nil, lowercase: nil, uppercase: nil, numbers: nil, special: nil)
+    character_counts = populate_missing_counts({
       lowercase: lowercase,
       uppercase: uppercase,
       numbers: numbers,
       special: special,
-    }
-    
-    generated_options = user_defined_options.select { |k, v| v.nil? }
+    }, length)
 
-    generated_options.keys.each_with_index do |key, index|
-      if index == generated_options.keys.length - 1
-        generated_options[key] = remaining_characters
+    characters = generate_character_set(character_counts[:lowercase]) { UPPERCASE.sample } +
+                 generate_character_set(character_counts[:uppercase]) { LOWERCASE.sample } +
+                 generate_character_set(character_counts[:numbers]) { NUMBERS.sample } +
+                 generate_character_set(character_counts[:special]) { SPECIAL.sample }
+
+    characters.shuffle.join
+  end
+
+  def self.populate_missing_counts(character_counts, max_length)
+    remaining_characters = calculate_remaining_characters(character_counts, max_length)
+    raise InvalidOptions if remaining_characters < 0
+
+    missing_counts = character_counts.select { |_, value| value.nil? }
+    missing_counts.keys.each_with_index do |key, index|
+      if index == missing_counts.keys.length - 1
+        character_counts[key] = remaining_characters
       else
-        generated_options[key] = generate_character_count(remaining_characters)
+        character_counts[key] = generate_character_count(remaining_characters)
       end
 
-      remaining_characters -= generated_options[key]
+      remaining_characters -= character_counts[key]
     end
 
-    raise StandardError if remaining_characters > 0
+    raise InvalidOptions if remaining_characters > 0
 
-    user_defined_options.merge(generated_options)
+    character_counts
+  end
+
+  def self.calculate_remaining_characters(character_counts, max_length)
+    current_total = character_counts.values.compact.inject(0, :+)
+
+    if max_length.nil?
+      max_length = (current_total > DEFAULT_LENGTH) ? current_total : DEFAULT_LENGTH
+    end
+
+    max_length - current_total
+  end
+
+  def self.generate_character_set(length)
+    set = []
+    while set.length < length
+      set << yield
+    end
+    set
   end
 
   def self.generate_character_count(remaining_characters)
--- spec/password_spec.rb
+++ spec/password_spec.rb
@@ -6,76 +6,87 @@ describe Password do
     srand(5)
   end
 
-  it "returns a password with 15 characters" do
-    password = Password.generate
-    _(password).must_equal "O0e8IRq5jXUiSH@"
-  end
-
   describe "length" do
+    it "returns a password with default of 15 characters" do
+      password = Password.generate
+      _(password).must_equal "m0E7hqQ5JrpIqh@"
+    end
+
     it "returns a password with a custom length" do
       password = Password.generate(length: 30)
-      _(password).must_equal "!6IXB2LV$qi8YZP7TH1S3j4R#GEJw@"
+      _(password).must_equal "£plhrhv99bq*Q6mI2q9J4m9!enW@hq"
+    end
+
+    it "allows passwords to be longer than the default based on other options" do
+      password = Password.generate(special: 30)
+      _(password).must_equal "%!%!@&$%%£@!&^&*!%^!*#!!£@$£**"
     end
   end
 
   describe "lowercase" do
     it "returns a password with an exact number of lowercase characters" do
       password = Password.generate(lowercase: 5)
-      _(password).must_equal "T9k1SEg70rIjx65"
+      _(password).must_equal "q0J7qeG5W17QhI1"
     end
 
     it "returns a password with zero lowercase characters" do
       password = Password.generate(lowercase: 0)
-      _(password).must_equal "9WI803^1J74Q@!&"
+      _(password).must_equal "74i%07w!q5*1j@0"
     end
   end
 
   describe "uppercase" do
     it "returns a password with an exact number of uppercase characters" do
       password = Password.generate(uppercase: 5)
-      _(password).must_equal "U9k1TEg70rJIx65"
+      _(password).must_equal "q0J7qeG5W17Qhi1"
     end
 
     it "returns a password with zero uppercase characters" do
       password = Password.generate(uppercase: 0)
-      _(password).must_equal "9wi803^1j74q@!&"
+      _(password).must_equal "74I%07W!Q5*1J@0"
     end
   end
 
   describe "numbers" do
     it "returns a password with an exact number of number characters" do
       password = Password.generate(numbers: 5)
-      _(password).must_equal "1gkEUT5KrJIx609"
+      _(password).must_equal "q0J7qeG5W1hQhi1"
     end
 
     it "returns a password with zero number characters" do
       password = Password.generate(numbers: 0)
-      _(password).must_equal "VwiIJT^PjSEq@!&"
+      _(password).must_equal "pWIhhq*mJqeQ!^@"
     end
   end
 
   describe "special" do
     it "returns a password with an exact number of special characters" do
       password = Password.generate(special: 5)
-      _(password).must_equal "U#k@TEgK!rJIx&^"
+      _(password).must_equal "q!J*qeG^W@hQhi@"
     end
 
     it "returns a password with zero special characters" do
       password = Password.generate(special: 0)
-      _(password).must_equal "VwiIJT9PjSEq061"
+      _(password).must_equal "pWIhhq7mJqeQ051"
     end
   end
 
   describe "multiple options" do
     it "returns a password supporting all options" do
       password = Password.generate(length: 12, lowercase: 3, uppercase: 3, numbers: 3, special: 3)
-      _(password).must_equal "9r*4XG&Rp8$d"
+      _(password).must_equal "*4P!9wgD!qO8"
     end
 
-    it "raises an error if options conflict with length" do
+    it "raises an error if options total is higher than length" do
       _(proc {
         Password.generate(length: 11, lowercase: 3, uppercase: 3, numbers: 3, special: 3)
-      }).must_raise StandardError
+      }).must_raise Password::InvalidOptions
+    end
+
+    it "raises an error if options total is lower than length" do
+      _(proc {
+        Password.generate(length: 13, lowercase: 3, uppercase: 3, numbers: 3, special: 3)
+      }).must_raise Password::InvalidOptions
     end
   end
 end
