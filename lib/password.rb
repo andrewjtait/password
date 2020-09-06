78141ac898d3b6f546f3afe274ec87327b783c6c
Andrew Tait <ajtait.uk@gmail.com>
2020-09-06 17:00:32 +0100

    Basic password generation
    In the first iteration we generate a password with a random mix of
    characters. Only the length of the password is configurable.

--- lib/password.rb
+++ lib/password.rb
@@ -1,5 +1,34 @@
 module Password
-  def self.generate
-    "password"
+  DEFAULT_LENGTH = 15
+  UPPERCASE = [*'A'..'Z'].freeze
+  LOWERCASE = [*"a".."z"].freeze
+  NUMBERS = [*0..9].freeze
+  SPECIAL = %w(! @ £ $ % ^ & * #).freeze
+
+  def self.generate(length: DEFAULT_LENGTH)
+    remaining_characters = length
+
+    lowercase_count = generate_character_count(remaining_characters)
+    remaining_characters -= lowercase_count
+
+    uppercase_count = generate_character_count(remaining_characters)
+    remaining_characters -= uppercase_count
+
+    numbers_count = generate_character_count(remaining_characters)
+    remaining_characters -= numbers_count
+
+    special_count = remaining_characters
+
+    characters = LOWERCASE.sample(lowercase_count) +
+                 UPPERCASE.sample(uppercase_count) +
+                 NUMBERS.sample(numbers_count) +
+                 SPECIAL.sample(special_count)
+
+    characters.shuffle.join
+  end
+
+  def self.generate_character_count(remaining_characters)
+    return 0 if remaining_characters == 0
+    rand(1..remaining_characters)
   end
 end
--- spec/password_spec.rb
+++ spec/password_spec.rb
@@ -2,7 +2,19 @@ require "minitest/autorun"
 require "password"
 
 describe Password do
-  it "returns a password" do
-    _(Password.generate).must_equal "password"
+  before do
+    srand(5)
+  end
+
+  it "returns a password with 15 characters" do
+    password = Password.generate
+    _(password).must_equal "O0e8IRq5jXUiSH@"
+  end
+
+  describe "length" do
+    it "returns a password with a custom length" do
+      password = Password.generate(length: 30)
+      _(password).must_equal "!6IXB2LV$qi8YZP7TH1S3j4R#GEJw@"
+    end
   end
 end
