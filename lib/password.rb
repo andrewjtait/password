b6ffe21de672988469970205dec367ad4695a5c5
Andrew Tait <ajtait.uk@gmail.com>
2020-09-07 10:13:44 +0100

    Remove redundant code comments
    Now that the code is easier to follow, there are perhaps too many
    comments that are in the way. This commit removes comments on any code
    that is self explanatory, leaving comments behind that add benefit.

--- lib/password.rb
+++ lib/password.rb
@@ -34,9 +34,7 @@ module Password
       raise InvalidOptionsError if !max_length.is_a?(Integer) || max_length < 1
     end
 
-    # Populate any nil values within the character_counts hash and validate them.
     def validate_character_counts
-      # Raise an error if a value is not valid:
       character_counts.values.compact.each do |count|
         raise InvalidOptionsError if !count.is_a?(Integer) || count < 0
       end
@@ -48,12 +46,9 @@ module Password
         character_counts[key] = last_item ? remaining_character_count : random_character_count
       end
 
-      # Raise an error if the total options are less than or more than the defined length:
       raise InvalidOptionsError if remaining_character_count != 0
     end
 
-    # Remaining characters is based on the sum of current character counts subtracted
-    # from the max length.
     def remaining_character_count
       current_total = character_counts.values.compact.inject(0, :+)
 
@@ -75,7 +70,6 @@ module Password
       max_length - current_total
     end
 
-    # Build and join sets of characters for each character type.
     def generate_characters
       generate_character_set(character_counts[:lowercase]) { LOWERCASE.sample } +
       generate_character_set(character_counts[:uppercase]) { UPPERCASE.sample } +
@@ -83,7 +77,6 @@ module Password
       generate_character_set(character_counts[:special]) { SPECIAL.sample }
     end
 
-    # Build an array of characters for the given length.
     def generate_character_set(length)
       set = []
       while set.length < length
@@ -92,8 +85,6 @@ module Password
       set
     end
 
-    # Return a random number for how many characters to use for the current
-    # character type based on the remaining characters available.
     def random_character_count
       # Reduce the maximum number a bit so that we have a better chance
       # of a good mixture of character types:
