7975c115c1facdefa802327b3561d6bdc2637ccf
Andrew Tait <ajtait.uk@gmail.com>
2020-09-07 10:27:12 +0100

    Refactor spec assertions so they are less brittle
    Previously I was asserting the exact output for each test, this approach
    proved to be too brittle as a minor modification to how the password
    is constructed results in all tests needing to be updated.
    
    The previous approach also relied too much on manual checks, in that a
    human would need to manually count the number of each character type in
    each test to determine if they were behaving correctly.
    
    The previous approach also relied on the randomness of the password
    generation to be locked, so that tests would behave the same on each
    run. This results in tests that are less robust.
    
    This commit refactors the tests so that I am instead checking the
    composition of each result. This means I no longer need to manually
    count characters in each test and the tests are more resilient.

--- spec/password_spec.rb
+++ spec/password_spec.rb
@@ -2,86 +2,104 @@ require "minitest/autorun"
 require "password"
 
 describe Password do
-  before do
-    srand(5)
-  end
+  UPPERCASE = %r([A-Z])
+  LOWERCASE = %r([a-z])
+  NUMBERS = %r([0-9])
+  SPECIAL = %r([@%!?*^&])
 
   describe "length" do
     it "returns a password with default of 15 characters" do
+      srand(5)
       password = Password.generate
       _(password).must_equal "P?H^MQj^*%eHQi@"
     end
 
     it "returns a password with a custom length" do
       password = Password.generate(length: 30)
-      _(password).must_equal "%P1HRHV%!4Q!q2MI9Q9j6M%@ENw&HQ"
+      _(password.length).must_equal 30
     end
 
     it "allows passwords to be longer than the default based on other options" do
       password = Password.generate(special: 30)
-      _(password).must_equal "*&^*&&&*@%%^@&^@?&%?@*^@?&!%?&"
+      _(password.length).must_equal 30
+      _(password.scan(SPECIAL).length).must_equal 30
     end
 
     it "returns the length of user defined options if all are defined" do
       password = Password.generate(lowercase: 2, uppercase: 0, numbers: 0, special: 0)
-      _(password).must_equal "do"
+      _(password.length).must_equal 2
+      _(password.scan(LOWERCASE).length).must_equal 2
     end
   end
 
   describe "lowercase" do
     it "returns a password with an exact number of lowercase characters" do
       password = Password.generate(lowercase: 5)
-      _(password).must_equal "1gjE0Q?7qHiw5@7"
+      _(password.length).must_equal 15
+      _(password.scan(LOWERCASE).length).must_equal 5
     end
 
     it "returns a password with zero lowercase characters" do
       password = Password.generate(lowercase: 0)
-      _(password).must_equal "7*8%07W*@J04Q^?"
+      _(password.length).must_equal 15
+      _(password.scan(LOWERCASE).length).must_equal 0
     end
   end
 
   describe "uppercase" do
     it "returns a password with an exact number of uppercase characters" do
       password = Password.generate(uppercase: 5)
-      _(password).must_equal "1gJE0Q?7qHIw5@7"
+      _(password.length).must_equal 15
+      _(password.scan(UPPERCASE).length).must_equal 5
     end
 
     it "returns a password with zero uppercase characters" do
       password = Password.generate(uppercase: 0)
-      _(password).must_equal "7*8%07w*@j04q^?"
+      _(password.length).must_equal 15
+      _(password.scan(UPPERCASE).length).must_equal 0
     end
   end
 
   describe "numbers" do
     it "returns a password with an exact number of number characters" do
       password = Password.generate(numbers: 5)
-      _(password).must_equal "1gJE0Q?7qHIw5@7"
+      _(password.length).must_equal 15
+      _(password.scan(NUMBERS).length).must_equal 5
     end
 
     it "returns a password with zero number characters" do
       password = Password.generate(numbers: 0)
-      _(password).must_equal "H*I%QHw*@jQEq^?"
+      _(password.length).must_equal 15
+      _(password.scan(NUMBERS).length).must_equal 0
     end
   end
 
   describe "special" do
     it "returns a password with an exact number of special characters" do
       password = Password.generate(special: 5)
-      _(password).must_equal "*gJE0Q?7qHIw%@^"
+      _(password.length).must_equal 15
+      _(password.scan(SPECIAL).length).must_equal 5
     end
 
     it "returns a password with zero special characters" do
       password = Password.generate(special: 0)
-      _(password).must_equal "HEI4QHw0q571j1Q"
+      _(password.length).must_equal 15
+      _(password.scan(SPECIAL).length).must_equal 0
     end
   end
 
   describe "multiple options" do
     it "returns a password supporting all options" do
       password = Password.generate(length: 12, lowercase: 3, uppercase: 3, numbers: 3, special: 3)
-      _(password).must_equal "9p4&WG@?Qo8d"
+      _(password.length).must_equal 12
+      _(password.scan(LOWERCASE).length).must_equal 3
+      _(password.scan(UPPERCASE).length).must_equal 3
+      _(password.scan(NUMBERS).length).must_equal 3
+      _(password.scan(SPECIAL).length).must_equal 3
     end
+  end
 
+  describe "validation" do
     it "raises an error if options total is higher than length" do
       _(proc {
         Password.generate(length: 11, lowercase: 3, uppercase: 3, numbers: 3, special: 3)
@@ -93,9 +111,7 @@ describe Password do
         Password.generate(length: 13, lowercase: 3, uppercase: 3, numbers: 3, special: 3)
       }).must_raise Password::InvalidOptionsError
     end
-  end
 
-  describe "validation" do
     it "raise an error for any invalid length value" do
       _(proc {
         Password.generate(length: 0)
