03b4920584e13e098387fde624c7a4a4ce417c29
Andrew Tait <ajtait.uk@gmail.com>
2020-09-07 20:50:48 +0100

    Refactor to remove instance variables
    The advantage of using a singleton means the module can make use of
    private methods and instance variables so that arguments do not need to
    be passed between methods.
    
    The downsides to this though are that instance variables are not as
    declarative and introduce a risk with thread safety as state is shared.
    
    To mitigate any potential issues, this commit removes instance variables
    and adds arguments to methods where needed.

--- lib/password.rb
+++ lib/password.rb
@@ -10,69 +10,79 @@ module Password
   SPECIAL   = %w(@ % ! ? * ^ &).freeze
 
   class << self
-    attr_reader :character_counts, :max_length
-
     def generate(length: nil, lowercase: nil, uppercase: nil, numbers: nil, special: nil)
-      @max_length = length
-      @character_counts = {
+      max_length = length
+      character_counts = {
         lowercase: lowercase,
         uppercase: uppercase,
         numbers: numbers,
         special: special,
       }
 
-      validate_length
-      validate_character_counts
+      validate_custom_options(character_counts, max_length)
 
-      generate_characters.shuffle.join
+      max_length ||= set_max_length(character_counts)
+      character_counts = set_character_counts(character_counts, max_length)
+
+      generate_characters(character_counts).shuffle.join
     end
 
     private
 
-    def validate_length
-      return if max_length.nil?
-      raise InvalidOptionsError if !max_length.is_a?(Integer) || max_length < 1
-    end
+    def validate_custom_options(character_counts, max_length)
+      unless max_length.nil?
+        raise InvalidOptionsError if !max_length.is_a?(Integer) || max_length < 1
+      end
 
-    def validate_character_counts
       character_counts.values.compact.each do |count|
         raise InvalidOptionsError if !count.is_a?(Integer) || count < 0
       end
+    end
 
-      raise InvalidOptionsError if remaining_character_count < 0
+    # Max length is based on:
+    #  1. The user defined length argument.
+    #  2. The total of user defined options (if higher than DEFAULT_LENGTH or user
+    #     has defined all possible options).
+    #  3. The DEFAULT_LENGTH.
+    def set_max_length(character_counts)
+      current_total = current_character_count(character_counts)
+      all_options_defined = !character_counts.values.any?(&:nil?)
+
+      if all_options_defined || (current_total > DEFAULT_LENGTH)
+        current_total
+      else
+        DEFAULT_LENGTH
+      end
+    end
+
+    def set_character_counts(character_counts, max_length)
+      raise InvalidOptionsError if remaining_character_count(character_counts, max_length) < 0
 
       # Populate any missing values:
       missing_counts = character_counts.select { |_, value| value.nil? }
       missing_counts.keys.each_with_index do |key, index|
         last_item = (index == missing_counts.keys.length - 1)
-        character_counts[key] = last_item ? remaining_character_count : random_character_count
-      end
-
-      raise InvalidOptionsError if remaining_character_count != 0
-    end
-
-    def remaining_character_count
-      current_total = character_counts.values.compact.inject(0, :+)
-
-      # Max length is based on:
-      #  1. The user defined length argument.
-      #  2. The total of user defined options (if higher than DEFAULT_LENGTH or user
-      #     has defined all possible options).
-      #  3. The DEFAULT_LENGTH.
-      if max_length.nil?
-        all_options_defined = !character_counts.values.any?(&:nil?)
-
-        if all_options_defined || (current_total > DEFAULT_LENGTH)
-          @max_length = current_total
+        if last_item
+          character_counts[key] = remaining_character_count(character_counts, max_length)
         else
-          @max_length = DEFAULT_LENGTH
+          character_counts[key] = random_character_count(character_counts, max_length)
         end
       end
 
-      max_length - current_total
+      raise InvalidOptionsError if remaining_character_count(character_counts, max_length) != 0
+
+      character_counts
     end
 
-    def generate_characters
+    def remaining_character_count(character_counts, max_length)
+      max_length - current_character_count(character_counts)
+    end
+
+    def current_character_count(character_counts)
+      character_counts.values.compact.inject(0, :+)
+    end
+
+    def generate_characters(character_counts)
       generate_character_set(character_counts[:lowercase]) { LOWERCASE.sample } +
       generate_character_set(character_counts[:uppercase]) { UPPERCASE.sample } +
       generate_character_set(character_counts[:numbers]) { NUMBERS.sample } +
@@ -87,10 +97,10 @@ module Password
       set
     end
 
-    def random_character_count
+    def random_character_count(character_counts, max_length)
       # Reduce the maximum number a bit so that we have a better chance
       # of a good mixture of character types:
-      maximum = (remaining_character_count * 0.6).ceil
+      maximum = (remaining_character_count(character_counts, max_length) * 0.6).ceil
       rand(0..maximum)
     end
   end
